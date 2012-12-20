391c5ab28a38c5a7162b5e19b08f386a3a90b7b9
Mike Meier <mike.meier@ibrows.ch>
2012-12-20 18:28:11 +0100

    create dummy deploy.yml

--- app/autoload.php
+++ app/autoload.php
@@ -7,10 +7,16 @@ $loader = require __DIR__.'/../vendor/autoload.php';
 // intl
 if (!function_exists('intl_get_error_code')) {
     require_once __DIR__.'/../vendor/symfony/symfony/src/Symfony/Component/Locale/Resources/stubs/functions.php';
-
     $loader->add('', __DIR__.'/../vendor/symfony/symfony/src/Symfony/Component/Locale/Resources/stubs');
 }
 
 AnnotationRegistry::registerLoader(array($loader, 'loadClass'));
+
+// set dummy deploy.yml for community checkouts
+// can be removed if deploy.yml already existing or when outcomment  "- { resource: deploy.yml }" in config.yml
+$deployYml = __DIR__.'/config/deploy.yml';
+if(!file_exists($deployYml)){
+    file_put_contents($deployYml, '# Dummy file for community checkouts - needed because import in config.yml');
+}
 
 return $loader;
